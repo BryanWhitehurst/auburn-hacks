10b2c2cbb7f2c2d9a2967912881dc2705ca32181
sigmodic <johnathon_lenz@live.com>
2020-02-09 00:35:47 +0000

    javascript

--- 2B1T.js
+++ 2B1T.js
@@ -1,5 +1,6 @@
 function Login(){
     username = document.getElementById('login').value
-    localStorage.setItem("username", username)
+    sessionStorage.setItem("username", username)
     window.location.href = "main_page.html" 
+    console.log(username)
 }
